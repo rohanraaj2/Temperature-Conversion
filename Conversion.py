dbadc2d3b0bffbd6df00a4a5df83be260000297e
Rohan Raj <rohan.raaj2@gmail.com>
2023-10-02 03:18:19 +0500

    basic done

--- Conversion.py
+++ Conversion.py
@@ -7,13 +7,15 @@ choice = int(input())
 print ("Enter temperature")
 temperature = int(input())
 
-def celsius_to_fahrenheit(temperature):
-  return ((temperature - 32) / 1.8)
-
 def fahrenheit_to_celsius(temperature):
-  return ((temperature * 1.8) + 32)
+  return round(((temperature - 32) / 1.8), 1)
+
+def celsius_to_fahrenheit(temperature):
+  return round(((temperature * 1.8) + 32), 1)
 
 if choice == 1:
-  celsius_to_fahrenheit(temperature)
+  result = celsius_to_fahrenheit(temperature)
+  print ("The resulting temperature is", result, "degree Fahrenheit")
 elif choice == 2:
-  fahrenheit_to_celsius(temperature)
+  result = fahrenheit_to_celsius(temperature)
+  print ("The resulting temperature is", result, "degree Celsius")
